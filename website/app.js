e253f836f5ea79413de1785987131a7c46eabd83
Filigram <130276494+Filigram@users.noreply.github.com>
2024-06-23 18:52:25 +0400

    Add some js code

--- website/app.js
+++ website/app.js
@@ -5,24 +5,63 @@ const main = document.querySelector("main")
 var i = 0
 
 function addServer() {
+    const num = (i + 1).toString()
     const divCase = document.createElement("div")
     divCase.classList.add("case")
     divCase.classList.add("server")
-    divCase.textContent = i
-    i++
+    divCase.id = "server-".concat(num)
 
     // Liste des choses a mettre dans un serveur
     // Le nom
+    const nameInput = document.createElement("input")
+    nameInput.type = "text"
+    nameInput.id = "name-input-".concat(num)
+    divCase.appendChild(nameInput)
+
     // Le path du folder
+    const localPathInput = document.createElement("input")
+    localPathInput.type = "text"
+    localPathInput.id = "path-input-".concat(num)
     
     // Les users
-    // Btn ajouter un user
-    // Pour un user
-    // Check box write and access
-    // Décoche access decoche aussi write
-    // Btn delete user
+    const separator = document.createElement("div")
+    separator.classList.add("separator")
+    divCase.appendChild(separator)
 
-    // Mode invité
+    // Btn ajouter un user
+    const addUserBtn = document.createElement("button")
+    addUserBtn.id = "add-user-".concat(num)
+    addUserBtn.classList.add("server-btn")
+    const userDiv = document.createElement("div")
+    userDiv.classList.add("column")
+    addUserBtn.addEventListener("click", () => {
+        // Pour un user
+        // Name of user
+        // Check box write and access
+        // Décoche access decoche aussi write
+        // Btn delete user
+
+        const currentUser = document.createElement("div")
+        currentUser.className = "current-user-div row"
+
+        const userName = document.createElement("input")
+        currentUser.appendChild(userName)
+
+        const writeCheckBox = document.createElement("input")
+        writeCheckBox.type = "checkbox"
+        writeCheckBox.checked = false
+        currentUser.appendChild(writeCheckBox)
+
+        const readCheckBox = document.createElement("input")
+        readCheckBox.type = "checkbox"
+        readCheckBox.checked = true
+        currentUser.appendChild(readCheckBox)
+
+        userDiv.appendChild(currentUser)
+    })
+
+    divCase.appendChild(addUserBtn)
+    divCase.appendChild(userDiv)
     
     main.insertBefore(divCase, addServerBtn.parentElement)
 }
