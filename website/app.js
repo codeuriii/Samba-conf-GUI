cb1e93996ff217d54fef3226fcd021aedab4bafe
Filigram <130276494+Filigram@users.noreply.github.com>
2024-06-23 17:47:44 +0400

    Add todo

--- website/app.js
+++ website/app.js
@@ -10,6 +10,19 @@ function addServer() {
     divCase.classList.add("server")
     divCase.textContent = i
     i++
+
+    // Liste des choses a mettre dans un serveur
+    // Le nom
+    // Le path du folder
+    
+    // Les users
+    // Btn ajouter un user
+    // Pour un user
+    // Check box write and access
+    // Décoche access decoche aussi write
+    // Btn delete user
+
+    // Mode invité
     
     main.insertBefore(divCase, addServerBtn.parentElement)
 }
